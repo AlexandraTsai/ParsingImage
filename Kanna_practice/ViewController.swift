ff72837e4044081bbd07fdf3e2cad53562e981b4
AlejandraTsai <alexandra@rolo.chat>
2021-06-05 14:57:30 +0800

    Initial Commit

---
+++ Kanna_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kanna_practice
+//
+//  Created by AlexandraTsai on 2021/6/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
